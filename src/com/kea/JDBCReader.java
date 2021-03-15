d795cb048fda0f101e7771a6eaf80f58a991e4a4
LingDingLauPau <kasperjakob@me.com>
2021-03-15 11:00:45 +0100

    preparedstatement jdbcreader

--- src/com/kea/JDBCReader.java
+++ src/com/kea/JDBCReader.java
@@ -1,10 +1,26 @@
 package com.kea;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class JDBCReader {
 
     public String readFromDB(String sql) {
         JDBCWriter.connectDB();
 
         return sql;
+    }
+
+    public String printTableFromDB(String table){
+        JDBCWriter.connectDB();
+
+        String printTable = "SELECT * FROM football_club.?;";
+        PreparedStatement statement = prepareStatement(printTable);
+        try {
+            statement.setString(1, table);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return printTable;
     }
 }
