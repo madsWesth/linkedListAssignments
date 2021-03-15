3a62434b86471603c904a68921ee0e9bd093148b
JimmyJambino <47789013+JimmyJambino@users.noreply.github.com>
2021-03-15 16:10:21 +0100

    compare

--- src/com/kea/PlayerLinkedList.java
+++ src/com/kea/PlayerLinkedList.java
@@ -4,6 +4,7 @@ public class PlayerLinkedList {
     //Dummy nodes
     public PlayerListNode front;
     public PlayerListNode end;
+    private Comparator comparator = new Comparator();
 
     public PlayerLinkedList() {
         front = new PlayerListNode(new PlayerListNode(null,front),null); // initializes the dummy nodes.
@@ -17,11 +18,11 @@ public class PlayerLinkedList {
         } else {
             int count = 1;
             PlayerListNode current = front;
-            String result = "[\n" + count + ".\n" + current.next.data.toString(); // fence TODO: Make a general toString? this one only displays age.
+            String result = "[\n" + count + ".\n" + current.next.data.toString(); // fence
             current = current.next;
             count++;
             while(current.next != end) {
-                result += "\n" + count + ".\n" + current.data.toString();
+                result += "\n" + count + ".\n" + current.next.data.toString();
                 current = current.next;
                 count++;
             }
@@ -95,35 +96,50 @@ public class PlayerLinkedList {
         }
     }
 
-    public void sortByAge(Comparator comparator) {
+    public void sortByAge() {
         //Keep running while next is not null
         //Run Comparator.compareType for current node to next
         // int compared = compareType(current, current.next)
         // switch (compared) case x: move bla bla
-        PlayerListNode current = front;
-        //TODO: need to check for list size 2, in which case current.next.next will always be null
-        while(current.next != end) { // checks if there is anything to swap with
-            int compare = comparator.compareAge(current.next, current.next.next);
+        PlayerListNode current = front.next; //TODO: Reminder that current is front.next here unlike other places
+        boolean hasSwapped = true;
+        while(hasSwapped) {
+            hasSwapped = false;
+            while(current.next != end) { // checks if there is anything to swap with
+                int compare = comparator.compareAge(current, current.next); // compares first element to second element
 
-            switch (compare) {
-                case 1: // swap
-                    PlayerListNode cont = current;
-                    PlayerListNode next = current.next; // saves middle part that is swapped left
-                    current.next = current.next.next; // moves reference to next node
-                    next.next = current; // moves next reference to next node
-                    front.next = next;
-                    break;
-                case 0:
-                    System.out.println(compare);
-                    break;
-                case -1:
-                    System.out.println(compare);
-                    current = current.next;
-                    break;
-                default:
-                    break;
+                switch (compare) {
+                    case 1: // swap
+                        PlayerListNode first = current; // the first element
+                        PlayerListNode second = current.next; // the second element
+
+                        PlayerListNode before = current.prev;
+                        PlayerListNode after = current.next.next;
+                        // Next setup
+                        second.next = first;
+                        current.next = after;
+                        before.next = second;
+
+                        // Prev setup
+
+                        current = current.next;
+                        hasSwapped = true;
+                        break;
+                    case 0:
+                        System.out.println(compare);
+                        current = current.next;
+                        break;
+                    case -1:
+                        System.out.println(compare);
+                        current = current.next;
+                        break;
+                    default:
+                        break;
+                }
             }
+            System.out.println(this.toString());
         }
+
     }
 
     public int size() {
